e25cd5e3cabe5847bd72253bddb4374d5fed136c
Pritam Agrawal <pritamagrawal95@gmail.com>
2020-09-25 12:28:06 +0530

    Practicing useState and destructuring to create a note app

--- src/index.js
+++ src/index.js
@@ -2,28 +2,66 @@ import React,{ useState } from 'react';
 import ReactDOM from 'react-dom';
 import * as serviceWorker from './serviceWorker';
 
-
-const App = (props) => {
-  const [count, setCount] = useState(props.count)
-
+const NoteApp =() => {
+  const [notes, setNotes] = useState([])
+  const [title, setTitle] = useState('')
+  const [body, setBody] = useState('')
+  const addNote = (e) => {
+    e.preventDefault()
+    setNotes([
+      ...notes,
+      { title, body }
+    ])
+    setTitle('')
+    setBody('')
+  }
+  const removeNote = (title) => {
+    setNotes(notes.filter((note) => note.title !== title))
+  }
   return (
     <div>
-      <p>The current count is {count}</p>
-      <button onClick={() => setCount(count+1)}>+1</button>
-      <button onClick={() => setCount(count-1)}>-1</button>
-      <button onClick={() => setCount(props.count)}>Reset</button>
+      <h1>Notes</h1>
+      {notes.map((note) => (
+        <div key={note.title}>
+          <h3>{note.title}</h3>
+          <h5>{note.body}</h5>
+          <button onClick={() => removeNote(note.title)}>x</button>
+        </div>
+      ))}
+      <p>Add Note</p>
+      <form onSubmit={addNote}>
+        <input value={title} onChange={(e) => setTitle(e.target.value)} />
+        <textarea value={body} onChange={(e) => setBody(e.target.value)} />
+        <button>Add Note</button>
+      </form>
     </div>
   )
 }
 
-App.defaultProps = {
-  count: 0
-}
+
+// const App = (props) => {
+//   const [count, setCount] = useState(props.count)
+//   const [text, setText] = useState('Set')
+
+//   return (
+//     <div>
+//       <p>The current {text || 'count'} is {count}</p>
+//       <button onClick={() => setCount(count+1)}>+1</button>
+//       <button onClick={() => setCount(count-1)}>-1</button>
+//       <button onClick={() => setCount(props.count)}>Reset</button>
+//       <input value={text} onChange={(e) => setText(e.target.value)} />
+//     </div>
+//   )
+// }
+
+// App.defaultProps = {
+//   count: 0
+// }
 
 
 ReactDOM.render(
   <React.StrictMode>
-    <App count={2} />
+    <NoteApp />
   </React.StrictMode>,
   document.getElementById('root')
 );
